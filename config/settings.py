976c78c01c99de34961c8bd2f57fd0c304975193
youichiro <cinnamon416@gmail.com>
2019-06-03 15:14:21 +0900

    fix

--- config/settings.py
+++ config/settings.py
@@ -87,6 +87,13 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
 # CustomUserモデルをAdminに設定
 AUTH_USER_MODEL = 'account.User'
 
