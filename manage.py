fd157f495a2d50c7d3bdb458f7cbd77f01d2b1a3
youichiro <cinnamon416@gmail.com>
2019-08-05 13:35:06 +0900

    fix

--- manage.py
+++ manage.py
@@ -4,6 +4,10 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+    from django.db.backends.mysql.schema import DatabaseSchemaEditor
+    DatabaseSchemaEditor.sql_create_table += " ROW_FORMAT=DYNAMIC"
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
