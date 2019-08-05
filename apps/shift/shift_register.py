57ebdac9b70542b32f470b2f5ff191c0a77c305c
youichiro <cinnamon416@gmail.com>
2019-08-05 14:00:43 +0900

    fix

--- apps/shift/shift_register.py
+++ apps/shift/shift_register.py
@@ -103,25 +103,20 @@ def register(sheet, sheet_id):
 
 
 def main():
-    # エクセルファイルの読み取り
-    sat_file = settings.BASE_DIR + '/static/xlsx/sat_shift.xlsx'
-    sun_file = settings.BASE_DIR + '/static/xlsx/sun_shift.xlsx'
-
-    # Workbooks
+    sat_file = 'static/xlsx/sat_shift.xlsx'
     sat_wb = openpyxl.load_workbook(sat_file)
-    sun_wb = openpyxl.load_workbook(sun_file)
-
-    # Worksheets
     sat_sun_sheet = sat_wb['晴']
     sat_rain_sheet = sat_wb['雨']
-    sun_sun_sheet = sun_wb['晴']
-    sun_rain_sheet = sun_wb['雨']
-
-    # Registration
     print('saving sat_sun_sheet...')
     register(sat_sun_sheet, sheet_id=2)
     print('saving sat_rain_sheet...')
     register(sat_rain_sheet, sheet_id=3)
+
+
+    sun_file = 'static/xlsx/sun_shift.xlsx'
+    sun_wb = openpyxl.load_workbook(sun_file)
+    sun_sun_sheet = sun_wb['晴']
+    sun_rain_sheet = sun_wb['雨']
     print('saving sun_sun_sheet...')
     register(sun_sun_sheet, sheet_id=4)
     print('saving sun_rain_sheet...')
