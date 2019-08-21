f2fdb67a37efb279e577036d7885cf60686d1fa3
youichiro <cinnamon416@gmail.com>
2019-08-22 01:10:08 +0900

    fix

--- apps/notification/admin.py
+++ apps/notification/admin.py
@@ -3,7 +3,7 @@ from apps.notification.models import DeviceToken
 
 
 class DeviceTokenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'token')
+    list_display = ('id', 'username', 'token', 'created_at', 'updated_at')
 
 
 admin.site.register(DeviceToken, DeviceTokenAdmin)
--- apps/notification/models.py
+++ apps/notification/models.py
@@ -5,6 +5,8 @@ class DeviceToken(models.Model):
     id = models.AutoField(primary_key=True)
     token = models.CharField('トークン', max_length=100)
     username = models.CharField('ユーザ名', max_length=30, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'device_tokens'
