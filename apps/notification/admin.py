d678bffcefdee63eb2cd338e0fa4b36972940cbb
youichiro <cinnamon416@gmail.com>
2019-08-21 23:55:33 +0900

    fix

--- apps/notification/admin.py
+++ apps/notification/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.notification.models import DeviceToken
 
-# Register your models here.
+
+class DeviceTokenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'token')
+
+
+admin.site.register(DeviceToken, DeviceTokenAdmin)
