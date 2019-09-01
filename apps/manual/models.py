d2016fbaf3d1612970fcdfe1312e0cd8c30bb462
youichiro <cinnamon416@gmail.com>
2019-09-02 03:31:46 +0900

    fix

--- apps/manual/models.py
+++ apps/manual/models.py
@@ -6,7 +6,7 @@ class Manual(models.Model):
     id = models.AutoField(primary_key=True)
     category = models.CharField('分類', max_length=30)
     title = models.CharField('タイトル', max_length=30)
-    url = models.URLField(max_length=100, null=True, blank=True)
+    url = models.URLField(max_length=1000, null=True, blank=True)
     order = models.IntegerField('順番', null=True, blank=True)
 
     class Meta:
